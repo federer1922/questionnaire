2bb52b9b5826151398d5f099e04c9f8573b50e19
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-02-18 21:32:14 +0100

    improvement of def run_questionnaire

--- spec/home_controller_spec.rb
+++ spec/home_controller_spec.rb
@@ -11,28 +11,62 @@ describe HomeController, type: :controller do
     question_1 = Question.create!(kind: "open_ended", content: "What is your name and how often do you cycle?", page: page_1)
     question_2 = Question.create!(kind: "date", content: "What is your date of birth?", page: page_1)
     question_3 = Question.create!(kind: "single_choice", content: "What kind of bike do you prefer?", select_possible_answers: "road,mountain,hybrid,different", page: page_1)
+
     
     page_2 = Page.create!(number: 2, questionnaire: questionnaire_1)
     question_4 = Question.create!(kind: "open_ended", content: "What do you like about cycling?", page: page_2)
     question_5 = Question.create!(kind: "date", content: "When was the last time you went for a bike ride?", page: page_2)
     question_6 = Question.create!(kind: "single_choice", content: "How long are yours bike rides?", select_possible_answers: "less than 10 miles,around 20 miles,more than 20 miles", page: page_2)
+    question_7 = Question.create!(kind: "single_choice", content: "Have you ever had your bike stolend?", select_possible_answers: "yes,no", page: page_2)
+  
     
-    post :run_questionnaire, params: { questionnaire_id: questionnaire_1.id, question_1_id: question_1.id, answer_first: "Arek, twice a week", question_2_id: question_2.id, answer_second: "#{Date.today}", question_3_id: question_3.id, answer_third: "hybrid" , question_4_id: question_4.id, answer_fourth: "The freedom, fun and fitness aspects", question_5_id: question_5.id, answer_fifth: "#{Date.yesterday}", question_6_id: question_6.id, answer_sixth: "more than 20 miles" }
-    
-    completing_survey = CompletingSurvey.first
 
+    post :run_questionnaire, params: { questionnaire_id: questionnaire_1.id }
+
+    completing_survey = CompletingSurvey.first
     expect(completing_survey.questionnaire).to eq questionnaire_1
+    #post :run_questionnaire, params: { questionnaire_id: questionnaire_1.id, question_1_id: question_1.id, answer_first: "Arek, twice a week", question_2_id: question_2.id, answer_second: "#{Date.today}", question_3_id: question_3.id, answer_third: "hybrid" , question_4_id: question_4.id, answer_fourth: "The freedom, fun and fitness aspects", question_5_id: question_5.id, answer_fifth: "#{Date.yesterday}", question_6_id: question_6.id, answer_sixth: "more than 20 miles" }
+    
+    post :run_questionnaire, params: { completing_survey_id: completing_survey.id, answers: { question_1.id => "Arek, twice a week", question_2.id => "#{Date.today}", question_3.id => "hybrid", question_4.id => "The freedom, fun and fitness aspects", question_5.id => "#{Date.yesterday}", question_6.id => "more than 20 miles", question_7.id => "yes" } }
+
     expect(question_1.answers.first.open_ended_question).to eq "Arek, twice a week"
-    expect(question_1.answers.first.completing_survey).to eq completing_survey
+    expect(question_1.page.number).to eq 1
     expect(question_2.answers.first.date_selection).to eq Date.today
-    expect(question_2.answers.first.completing_survey).to eq completing_survey
+    expect(question_2.page.number).to eq 1
     expect(question_3.answers.first.single_choice_question).to eq "hybrid"
-    expect(question_3.answers.first.completing_survey).to eq completing_survey
+    expect(question_3.page.number).to eq 1
     expect(question_4.answers.first.open_ended_question).to eq "The freedom, fun and fitness aspects"
-    expect(question_4.answers.first.completing_survey).to eq completing_survey
+    expect(question_4.page.number).to eq 2
     expect(question_5.answers.first.date_selection).to eq Date.yesterday
-    expect(question_5.answers.first.completing_survey).to eq completing_survey
+    expect(question_5.page.number).to eq 2
     expect(question_6.answers.first.single_choice_question).to eq "more than 20 miles" 
-    expect(question_6.answers.first.completing_survey).to eq completing_survey
+    expect(question_6.page.number).to eq 2
+    expect(question_7.answers.first.single_choice_question).to eq "yes" 
+    expect(question_7.page.number).to eq 2
+
+
+
+
+
+
+
+  #  # post :run_questionnaire, params: { completing_survey_id: completing_survey.id, page_id: page_2.id, answers: { question_4.id => "Arek, twice a week", question_5.id => "#{Date.today}", question_6.id => "hybrid" } }
+
+
+  #   completing_survey = CompletingSurvey.first
+
+  #   expect(completing_survey.questionnaire).to eq questionnaire_1
+  #   expect(question_1.answers.first.open_ended_question).to eq "Arek, twice a week"
+  #   expect(question_1.answers.first.completing_survey).to eq completing_survey
+  #   expect(question_2.answers.first.date_selection).to eq Date.today
+  #   expect(question_2.answers.first.completing_survey).to eq completing_survey
+  #   expect(question_3.answers.first.single_choice_question).to eq "hybrid"
+  #   expect(question_3.answers.first.completing_survey).to eq completing_survey
+  #   expect(question_4.answers.first.open_ended_question).to eq "The freedom, fun and fitness aspects"
+  #   expect(question_4.answers.first.completing_survey).to eq completing_survey
+  #   expect(question_5.answers.first.date_selection).to eq Date.yesterday
+  #   expect(question_5.answers.first.completing_survey).to eq completing_survey
+  #   expect(question_6.answers.first.single_choice_question).to eq "more than 20 miles" 
+  #   expect(question_6.answers.first.completing_survey).to eq completing_survey
   end
 end
